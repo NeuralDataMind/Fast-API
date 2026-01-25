dcc0300bf604de6f5931cb9e9f18b99c3478aae8
Mallikarjun Reddy Bardipuram <sunnygalaxym12@gmail.com>
2026-01-25 23:44:57 +0530

    created schemas and db

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate, PostResponse
 
 app = FastAPI()
 
@@ -55,6 +56,15 @@ def get_all_posts(limit: int = None):
     return text_posts
 
 # Endpoint to get a specific post
-@app.get("/post/{post_id}")
-def get_post(post_id: int):
-    return text_posts.get(post_id, {"error": "Post not found"})
+@app.get("/post/{id}")
+def get_post(id: int) -> PostResponse:
+    if id not in text_posts:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return text_posts.get(id)
+
+@app.post("/posts")
+def create_post(post: PostCreate) -> PostResponse:
+    new_post = {"title": post.title, "content": post.content}
+    text_posts[max(text_posts.keys()) + 1] = new_post
+
+    return new_post
